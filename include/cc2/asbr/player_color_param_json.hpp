afb846990d8c8468eb81dc6c9a9b701e2079e8cc
Kojo Bailey <kojomolojo@gmail.com>
2025-07-29 16:05:14 +0530

    Various

--- include/cc2/asbr/player_color_param_json.hpp
+++ include/cc2/asbr/player_color_param_json.hpp
@@ -3,27 +3,30 @@
 
 #include <cc2/asbr/player_color_param.hpp>
 
-using namespace cc2;
+#include <kojo/logger.hpp>
+
+namespace cc2 {
 
 template<>
 class json_serializer<asbr::player_color_param> {
 public:
     static asbr::player_color_param read(const nlohmann::ordered_json& json) {
         asbr::player_color_param result;
+        kojo::logger log{"PlayerColorParam JSON Reader"};
 
         for (const auto& [key, value] : json.items()) {
             if (key == "Version" || key == "Filetype") continue;
 
-            // if (!value.is_string()) error::print(
-            //     nucc::status_code::json_value,
-            //     std::format("JSON data for entry \"{}\" is not a valid hex code.", key),
-            //     "Ensure all hex codes are strings with the format \"#RRGGBB\"."
-            // );
-            // if (value.template get<std::string>().length() != 7) error::print(
-            //     nucc::status_code::json_value,
-            //     std::format("JSON data for entry \"{}\" is not a valid hex code.", key),
-            //     "Ensure all hex codes are strings with the format \"#RRGGBB\". Alpha channel is not supported."
-            // );
+            if (!value.is_string()) log.error(
+                kojo::logger::status::bad_value,
+                std::format("JSON data for entry \"{}\" is not a valid hex code.", key),
+                "Ensure all hex codes are strings with the format \"#RRGGBB\"."
+            );
+            if (value.template get<std::string>().length() != 7) log.error(
+                kojo::logger::status::bad_value,
+                std::format("JSON data for entry \"{}\" is not a valid hex code.", key),
+                "Ensure all hex codes are strings with the format \"#RRGGBB\". Alpha channel is not supported."
+            );
 
             asbr::player_color_param::entry entry_buffer;
             entry_buffer.character_id = key.substr(0, 4) + "0" + key.at(5);
@@ -40,7 +43,7 @@ public:
     static nlohmann::ordered_json write(const asbr::player_color_param& param) {
         nlohmann::ordered_json result;
 
-        result["Version"] = 250718;
+        result["Version"] = 250729;
         result["Filetype"] = "PlayerColorParam";
 
         for (auto& [key, entry] : param.entries) {
@@ -50,5 +53,7 @@ public:
         return result;
     }
 };
+
+}
 
 #endif // KOJO_CC2_ASBR_PLAYERCOLORPARAMJSON
